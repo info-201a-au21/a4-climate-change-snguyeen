a961cdf69b87a13689ff8a487cf180580d6b013e
Scarlett Nguyen <scarlettnguyen@Scarletts-MacBook-Pro.local>
2021-12-06 15:13:15 -0800

    tbu3

--- app.R
+++ app.R
@@ -4,7 +4,7 @@ source("app_ui.R")
 source("app_server.R")
 library("rsconnect")
 
-rsconnect::setAccountInfo(name='snguyeen', token='092BDA19DE263B6BEC95469DD483EACE', 
-                          secret='RKiizBTGLEPWyxfCGSxe72vpZzy9ajldc8FyWekd')
+#rsconnect::setAccountInfo(name='snguyeen', token='092BDA19DE263B6BEC95469DD483EACE', 
+ #                         secret='RKiizBTGLEPWyxfCGSxe72vpZzy9ajldc8FyWekd')
 
 shinyApp(ui = ui, server = server)
